eb028457488c88a6fec0949d352118e2b2b79e72
Anusha <sweet.anosha5@gmail.com>
2020-08-19 12:06:20 -0700

    CLI updated

--- src/CLI/main.py
+++ src/CLI/main.py
@@ -1,58 +1,16 @@
+import shutil
+
 import winshell
 import os
 
-def beginning():
-    r = list(winshell.recycle_bin())
-
-    class DeletedObject:
-        def __init__(self, obj, typeOfObj):
-            self.obj = obj
-            self.typeOfObj = typeOfObj
-
-    dictOfObjs = {}
-
-
-    fmt = "{0:3}|{1:7}|{2:100}"
-    for i,v in enumerate(r):
-        sr = i+1
-        total = v.original_filename().split('\\')
-        okPath = "\\\\".join(total)
-        v.undelete()
-
-        if os.path.isdir(okPath):
-            typee = "Folder"
-        elif os.path.isfile(okPath):
-            typee = "File"
-        winshell.delete_file(okPath)
-        dictOfObjs[sr] = DeletedObject(v,typee)
-
-        print(fmt.format(sr,typee,total[-1]))
-    selectItem(dictOfObjs)
-
-
-# print("dict", dictOfObjs[1].obj)
-def selectItem(dictOfObjs):
-    n = eval(input("Enter Number of item to be opened:  "))
+def ClearScreen():
     try:
-        selected = dictOfObjs[n]
-        print("----", selected.obj)
-        x = selected.obj.original_filename().split("\\")
-        selected_path = "\\\\".join(x)
-        winshell.undelete(selected.obj.original_filename())
-        print("NAME: ", x[-1])
-        print("PATH: ",selected_path)
-        if os.path.isfile(selected_path):
-            print("TYPE: FILE")
-            print("SIZE: ", os.path.getsize(selected_path))
-        elif os.path.isdir(selected_path):
-            print("TYPE: FOLDER")
-            print("SIZE: ", get_dir_size(selected_path))
-        winshell.delete_file(selected.obj.original_filename())
+        os.system("cls")
     except:
-        print("Enter a valid serial number")
-        selectItem(dictOfObjs)
+        os.system("clear")
+    print("------------------------ TRASH VISUALISER CLI ------------------------")
 
-def get_dir_size(self,start_path='.'):
+def get_dir_size(self, start_path='.'):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
         for f in filenames:
@@ -63,5 +21,218 @@ def get_dir_size(self,start_path='.'):
 
     return total_size
 
-#-------------DRIVER CODE-------------
-beginning()
+class DeletedObject:
+    def __init__(self, obj, typeOfObj, name, sahiPath, size, addOn):
+        self.obj = obj
+        self.typeOfObj = typeOfObj
+        self.name = name
+        self.sahiPath = sahiPath
+        self.size = size
+        self.addOn = addOn
+
+def Beginning(dictionary):
+    ClearScreen()
+    if dictionary is None:
+        listOfItems=list(winshell.recycle_bin())
+
+        dictOfObjects = {}
+        fmt = "{0:3} | {1:8} | {2:10}"
+        print(fmt.format("Sr:","Type:", "Name:"))
+        for i, v in enumerate(listOfItems):
+            serialNo = i+1
+            path = v.original_filename()
+            listOfPath = path.split("\\")
+            newPath = "\\\\".join(listOfPath)
+            winshell.undelete(path)
+            name = listOfPath[-1]
+            if os.path.isdir(newPath) == True:
+                type = "Folder"
+                size = get_dir_size(newPath)
+                listOfFolderItems = os.listdir(newPath)
+                noOfFolder = 0
+                noOfFile = 0
+                for i in listOfFolderItems:
+                    checkpath = newPath+ '\\\\' + i
+                    if os.path.isdir(checkpath) == True:
+                        noOfFolder +=1
+                    elif os.path.isfile(checkpath) == True:
+                        noOfFile +=1
+                dictOfObjects[serialNo] = DeletedObject(v, type , name, newPath, "{0:.1f}".format(size/1024), [noOfFile, noOfFolder])
+            elif os.path.isfile(newPath) == True:
+                type = "file"
+                size = os.path.getsize(newPath)
+                dictOfObjects[serialNo]= DeletedObject(v, type, name, newPath, "{0:.1f}".format(size/1024), [])
+            winshell.delete_file(path)
+            print(fmt.format(serialNo,type,name))
+        SelectedItem(dictOfObjects)
+    else:
+        fmt = "{0:3} | {1:8} | {2:10}"
+        print(fmt.format("Sr:", "Type:", "Name:"))
+        for k,v in dictionary.items():
+            print(fmt.format(k, v.typeOfObj, v.name))
+        SelectedItem(dictionary)
+
+
+def SelectedItem(dict):
+    print("Enter serial no of the file/folder you want to select")
+    n = eval(input("Enter: "))
+    ClearScreen()
+    try:
+        Obj= dict[n]
+        print("Name: ", Obj.name )
+        print("Path: ", Obj.obj.original_filename())
+        print("Type: ", Obj.typeOfObj)
+        print("Size: ", Obj.size)
+        if Obj.addOn == []:
+            pass
+        else:
+            print("No of files: ",Obj.addOn[0])
+            print("No of Folders: ", Obj.addOn[1])
+        OperationOnItem(Obj, dict)
+
+    except:
+        print("Please enter a valid serial no.")
+        SelectedItem(dict)
+
+def OperationOnItem(Obj, dict):
+    print(" Press O to open the ", Obj.typeOfObj, " \n Press D to delete the ", Obj.typeOfObj,
+          " \n Press R to restore the ", Obj.typeOfObj, "\n Press B to go back")
+    raw_input = input("Enter: ")
+    if raw_input == "O" or raw_input == "o":
+        if Obj.typeOfObj == "file":
+            ClearScreen()
+            OpenFile(Obj,dict)
+        elif Obj.typeOfObj == "Folder":
+            ClearScreen()
+            OpenFolder(Obj,dict)
+    elif raw_input == "D" or raw_input == "d":
+        DeleteItem(Obj)
+    elif raw_input == "R" or raw_input == "r":
+        RestoreItem(Obj)
+    elif raw_input == "B" or raw_input == "b":
+        Beginning(dict)
+
+
+def OpenFile(obj, dict):
+    try:
+        winshell.undelete(obj.obj.original_filename())
+        file= open(obj.sahiPath, 'r')
+        R=file.read()
+        file.close()
+        winshell.delete_file(obj.obj.original_filename())
+        print(R)
+        print("Press any key to go back")
+        key= input("Enter: ")
+        if key != '':
+            ClearScreen()
+            OperationOnItem(obj, dict)
+    except:
+        ClearScreen()
+        print("This file can not be opened ")
+        OperationOnItem(obj, dict)
+
+
+
+def DeleteItem(obj):
+    winshell.undelete(obj.obj.original_filename())
+    if obj.typeOfObj == "File":
+        os.remove(obj.sahiPath)
+    elif obj.typeOfObj == "Folder":
+        shutil.rmtree(obj.sahiPath)
+    Beginning(None)
+
+def RestoreItem(obj):
+    winshell.undelete(obj.obj.original_filename())
+    Beginning(None)
+
+def OpenFolder(obj, dict):
+    winshell.undelete(obj.obj.original_filename())
+    listOfDirs = os.listdir(obj.sahiPath)
+    winshell.delete_file(obj.obj.original_filename())
+    fmt = "{0:3} | {1:8} | {2:10}"
+    print(fmt.format("Sr:", "Type:", "Name:"))
+    Sr=0
+    for i in listOfDirs:
+        Sr+=1
+        if "." in i:
+            type = "file"
+        else:
+            type = "Folder"
+        print(fmt.format(Sr, type, i))
+    print(" Enter serial no to open any file/folder \n Press B to go back ")
+    n = input("Enter: ")
+    if str(n) == "B" or str(n) == "b":
+        Beginning(dict)
+    else:
+        try:
+            n= eval(n)
+            name = listOfDirs[n-1]
+            path = obj.sahiPath + "\\\\" + listOfDirs[n - 1]
+            if "." in name:
+                ClearScreen()
+                OpenFileFromFolder(obj, path, dict)
+            else:
+                ClearScreen()
+                OpenFolderFromFolder(obj, path, dict)
+        except:
+            ClearScreen()
+            print(" Enter a valid serial no ")
+            OpenFolder(obj, dict)
+
+def OpenFileFromFolder(obj, filepath, dict):
+    try:
+        winshell.undelete(obj.obj.original_filename())
+        file= open(filepath, "r")
+        R= file.read()
+        file.close()
+        winshell.delete_file(obj.obj.original_filename())
+        print(R)
+        print(" Press any key to go back ")
+        key = input(" Enter: ")
+        if key:
+            ClearScreen()
+            OpenFolderFromFolder(obj, "\\\\".join(filepath.split("\\\\")[:-1]), dict)
+    except:
+        ClearScreen()
+        print(" This file can not be opened ")
+        OpenFolderFromFolder(obj, "\\\\".join(filepath.split("\\\\")[:-1]), dict)
+
+def OpenFolderFromFolder(obj, folderpath, dict):
+    winshell.undelete(obj.obj.original_filename())
+    listOfDirs = os.listdir(folderpath)
+    winshell.delete_file(obj.obj.original_filename())
+    fmt = "{0:3} | {1:8} | {2:10}"
+    print(fmt.format("Sr:", "Type:", "Name:"))
+    Sr = 0
+    for i in listOfDirs:
+        Sr += 1
+        if "." in i:
+            type = "file"
+        else:
+            type = "Folder"
+        print(fmt.format(Sr, type, i))
+    print(" Enter serial no to open any file/folder \n Press B to go back ")
+    n= eval(input("Enter: "))
+    if str(n) == "B" or str(n) == "b":
+        if obj.sahiPath == folderpath:
+            Beginning(dict)
+        else:
+            ClearScreen()
+            OpenFolderFromFolder(obj, "\\\\".join(folderpath.split("\\\\")[:-1]), dict)
+    else:
+        try:
+            name = listOfDirs[n - 1]
+            path = folderpath + "\\\\" + listOfDirs[n - 1]
+            if "." in name:
+                ClearScreen()
+                OpenFileFromFolder(obj, path, dict)
+            else:
+                ClearScreen()
+                OpenFolderFromFolder(obj, path, dict)
+        except:
+            ClearScreen()
+            print(" Enter a valid serial no ")
+            OpenFolderFromFolder(obj, folderpath, dict)
+
+
+Beginning(None)
